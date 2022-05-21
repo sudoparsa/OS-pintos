a6bea63aa720add596606b35c81d3f514e8c87df
sudoparsa <sp.hoseiny@gmail.com>
2022-05-21 20:29:01 +0430

    Implement buffer cache cache.c cache.h

---
+++ pintos/src/filesys/cache.c
@@ -0,0 +1,102 @@
+#include <debug.h>
+#include <string.h>
+#include "cache.h"
+#include "filesys/filesys.h"
+
+
+#define NOTFOUND -1 
+
+
+void
+cache_init (void)
+{
+  lock_init(&cache_list_lock);
+  list_init(&cache_LRU);
+  
+  for (int i=0; i < CACHE_SIZE; i++)
+    {
+      lock_init(&cache[i].cache_lock);
+      cache[i].dirty = false;
+      cache[i].valid = false;
+      list_push_back (&cache_LRU, &(cache[i].cache_elem));
+    }
+}
+
+
+void
+flush_block (struct block *fs_device, struct cache_block *LRU_block)
+{
+  block_write(fs_device, LRU_block->sector_num, LRU_block->data);
+  LRU_block->dirty = false;
+}
+
+
+int
+get_cache_index(block_sector_t sector)
+{
+  for (int i=0; i < CACHE_SIZE; i++)
+    if(cache[i].valid && cache[i].sector_num == sector)
+      return i;
+  return NOTFOUND;
+}
+
+
+struct cache_block *
+get_cache_block (struct block *fs_device, block_sector_t sector)
+{
+  int index = get_cache_index (sector);
+  struct cache_block* LRU_block;
+  lock_acquire (&cache_list_lock);
+  if (index == NOTFOUND)
+    {
+      LRU_block = list_entry (list_pop_front (&cache_LRU), struct cache_block, cache_elem);
+      lock_acquire (&LRU_block->cache_lock);
+
+      if (LRU_block->valid && LRU_block->dirty)
+        flush_block (fs_device, LRU_block);
+
+      block_read (fs_device, sector, LRU_block->data);
+      LRU_block->valid = true;
+      LRU_block->dirty = false;
+      list_push_back (&cache_LRU, &LRU_block->cache_elem);
+
+      lock_release (&LRU_block->cache_lock);
+    }
+  else
+    {
+      list_remove (&cache[index].cache_elem);
+      list_push_back (&cache_LRU, &cache[index].cache_elem);
+      LRU_block = &cache[index];
+    }
+
+  lock_release (&cache_list_lock);
+  
+  return LRU_block;
+}
+
+
+void
+cache_read (struct block *fs_device, block_sector_t sector_idx, void *buffer, off_t offset, int chunk_size)
+{
+  ASSERT (fs_device != NULL);
+  ASSERT (offset >= 0 && chunk_size >= 0 && (offset + chunk_size) <= BLOCK_SECTOR_SIZE);
+
+  struct cache_block* cb = get_cache_block(fs_device, sector_idx);
+  lock_acquire(&cb->cache_lock);
+  memcpy(buffer, &(cb->data[offset]), chunk_size);
+
+  lock_release(&cb->cache_lock);
+}
+
+
+void cache_write (struct block *fs_device, block_sector_t sector_idx, void *buffer, off_t offset, int chunk_size)
+{        
+  ASSERT (fs_device != NULL);
+  ASSERT (offset >= 0 && chunk_size >= 0 && (offset + chunk_size) <= BLOCK_SECTOR_SIZE);
+
+  struct cache_block* cb = get_cache_block (fs_device, sector_idx);
+  lock_acquire(&cb->cache_lock);
+  memcpy(&(cb->data[offset]), buffer, chunk_size);
+  cb->dirty = true;
+  lock_release(&cb->cache_lock);
+}
---
+++ pintos/src/filesys/cache.h
@@ -0,0 +1,36 @@
+#ifndef FILESYS_CACHE_H
+#define FILESYS_CACHE_H
+
+#include "off_t.h"
+#include "devices/block.h"
+#include "threads/synch.h"
+#include "lib/kernel/list.h"
+
+
+#define CACHE_SIZE 64
+
+
+struct cache_block
+  {
+    block_sector_t sector_num;
+    bool valid;
+    bool dirty;
+    char data[BLOCK_SECTOR_SIZE];
+    struct list_elem cache_elem;
+    struct lock cache_lock;
+  };
+
+
+struct cache_block cache[CACHE_SIZE];
+struct list cache_LRU;
+struct lock cache_list_lock;
+
+
+void cache_init (void);
+void cache_read (struct block *fs_device, block_sector_t sector_idx, void *buffer, off_t offset, int chunk_size);
+void cache_write (struct block *fs_device, block_sector_t sector_idx, void *buffer, off_t offset, int chunk_size);
+void flush_block (struct block *fs_device, struct cache_block *LRU_block);
+int get_cache_index(block_sector_t sector);
+struct cache_block *get_cache_block (struct block *fs_device, block_sector_t sector);
+
+#endif /* filesys/cache.h */
